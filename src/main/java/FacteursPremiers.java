819dbbc806c54725683db2115c79cd08526a21b2
Alexandre Ferreira <ferreira64240@hotmail.com>
2023-10-06 14:59:51 +0200

    Début du tp2 sur les tests unitaires

---
+++ src/main/java/FacteursPremiers.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class FacteursPremiers {
+    public Set<Integer> generate(int n){
+        Set<Integer> facteursPremiers = new HashSet<>();
+        int diviseur = 2;
+
+        while (n > 1) {
+            while (n % diviseur == 0) {
+                facteursPremiers.add(diviseur);
+                n /= diviseur;
+            }
+            diviseur++;
+        }
+        return facteursPremiers;
+
+    }
+}
